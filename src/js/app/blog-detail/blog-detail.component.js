3d355c13dfd96be68442d9f1620b9f33bcc8f92c
Oscar6 <oscar.m1024@gmail.com>
2019-06-12 21:06:51 -0500

    Adding & Submitting comments - Form and ngSubmit

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -16,10 +16,25 @@ component("blogDetail", {
         if (post.id == $routeParams.id) {
           $scope.notFound = false;
           $scope.post = post;
+          resetReply()
         }
         // console.log(post);
       })
     })
+    // Adding & Submitting Comments
+    $scope.addReply = function() {
+      console.log($scope.reply);
+      $scope.post.comments.push($scope.reply)
+      resetReply()
+    }
+
+    function resetReply() {
+      $scope.reply = {
+        "id": $scope.post.comments.length + 1,
+        "text": "",
+      }
+    }
+
         if ($scope.notFound) {
             console.log("Not Found");
             $location.path("/");
