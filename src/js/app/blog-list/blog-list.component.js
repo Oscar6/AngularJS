0df89662ffc718379f5d917d28e0a3d4a087077d
Oscar6 <oscar.m1024@gmail.com>
2019-06-19 18:58:57 -0500

    Angular Columns, Post Filter, Console search query

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,19 +1,72 @@
 "use strict";
 
 // Self contained module
-angular.module("blogList").
-component("blogList", {
-//   template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
-  templateUrl: './templates/blog-list.html',
-  controller: function(Post, $location, $routeParams, $rootScope, $scope){
-    $scope.goToItem = function(post){
-      $rootScope.$apply(function(){
-        $location.path("/blog/" + post.id)
-      })
-    }
-// Mapping to a RESTful API using $resource
+angular.module("blogList").component("blogList", {
+  //   template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+  templateUrl: "./templates/blog-list.html",
+  controller: function(Post, $location, $routeParams, $rootScope, $scope) {
+    $scope.goToItem = function(post) {
+      $rootScope.$apply(function() {
+        $location.path("/blog/" + post.id);
+      });
+    };
+    // Mapping to a RESTful API using $resource
 
-    $scope.items = Post.query();
+    //     $scope.items = Post.query();
+    //   }
+    // });
+
+    // Adding JS to adjust columns
+
+    $scope.changeCols = function(number) {
+      if (angular.isNumber(number)) {
+        $scope.numCols = number
+      } else {
+        $scope.numCols = 2
+      }
+      setupCol($scope.items, $scope.numCols)
+    }
+    // Watches for a search query in console
+    $scope.loadingQuery = false
+    $scope.$watch(function() {
+      console.log($scope.query)
+      if($scope.query) {
+        $scope.loadingQuery = true
+        $scope.cssClass = 'col-sm-12'
+      } else {
+        if ($scope.loadingQuery) {
+          setupCol($scope.items, 2)
+          $scope.loadingQuery = false
+        }
+      }
+    })
+
+    function setupCol(data, number) {
+      if (angular.isNumber(number)) {
+        $scope.numCols = number;
+      } else {
+        $scope.numCols = 2;
+      }
+      $scope.cssClass = "col-sm-" + 12 / $scope.numCols;
+      $scope.items = data;
+      $scope.colItems = chunkArrayInGroups(data, $scope.numCols);
+    }
+
+    Post.query(
+      function(data) {
+        setupCol(data, 4);
+      }, function(errorData) {
+
+      });
+    function chunkArrayInGroups(array, unit) {
+      var results = [],
+        length = Math.ceil(array.length / unit);
+
+      for (var i = 0; i < length; i++) {
+        results.push(array.slice(i * unit, (i + 1) * unit));
+      }
+      return results;
+    }
   }
 });
 
